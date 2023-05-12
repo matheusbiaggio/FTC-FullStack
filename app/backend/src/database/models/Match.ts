d2907e1be1b0ede9494c9752051816057a47d017
Matheus Biaggio <matheusbiaggio@gmail.com>
2023-05-12 17:22:08 -0300

    Implementação do modelo e migration para matches no BD

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,48 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Match extends Model {
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoal: number;
+  declare inProgress: boolean;
+}
+
+Match.init({
+  homeTeamId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  awayTeamId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  inProgress: {
+    allowNull: false,
+    type: DataTypes.BOOLEAN,
+  },
+}, {
+  modelName: 'matches',
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+export default Match;
